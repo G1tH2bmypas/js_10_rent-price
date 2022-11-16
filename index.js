21cc6d8eb001bf20ee3190c21050526a02c0a741
Tekyto <Tekyto2014@gmail.com>
2022-11-16 18:33:41 +0200

    js_10_rent-price complete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function rentPrice(days) {
-	// write your code here
+	return days <= 3 ? days * 25 : days >= 4 && days <= 7 ? (days - 3) * 20 + (3 * 25) : (days - 7) * 15 + 3 * 25 + 4 * 20;
 }
 
 module.exports = rentPrice;
